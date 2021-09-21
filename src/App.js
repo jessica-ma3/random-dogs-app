2b0771713af16a670dd4734019b361f7eaf3fba9
jessica-ma3 <jessica@github.no.reply.com>
2021-09-22 01:39:20 +1000

    Complete loading more files, styles still needs to be improved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from "react";
 import axios from "axios";
 import './App.css';
-import { Layout, Menu, Breadcrumb, Row, Col } from 'antd';
+import { Layout, Menu, Breadcrumb, Button } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
@@ -18,18 +18,50 @@ export default function App() {
   const fileTypes = ['png', 'jpg', 'jpeg', 'bmp', 'gif', 'webp', 'svg', 'tiff', 'mp4', 'mov', 'webm', 'wmv', 'avi', 'avchd'];
   const imageTypes = ['png', 'jpg', 'jpeg', 'bmp', 'gif', 'webp', 'svg', 'tiff'];
   const videoTypes = ['mp4', 'mov', 'webm', 'wmv', 'avi', 'avchd'];
+  
+  // initial number of files
+  const INITIAL_NUM = 8;
 
   const getFileType = (fileUrl) => {
     let partitions = fileUrl.split('.');
     return partitions[partitions.length - 1].toLowerCase();
- }
+  }
+
+  // fetch specified 'number' of files
+  const fetchFiles = (promises, files, number) => {
+    var fileUrl = '';
+    for (let i=0; i<number;i++) {
+    promises.push(axios.get(`https://random.dog/woof.json`)
+      .then(res => { 
+        //setIsLoaded(true); 
+        //setFileUrl(res.data.url);
+        //console.log(fileUrl);
+        fileUrl = res.data.url;
+        console.log(fileUrl);
+        console.log(getFileType(fileUrl));
+        if (fileTypes.includes(getFileType(fileUrl))) {
+          files.push(fileUrl);
+          console.log(files);
+        }
+        //console.log(files.length);
+        
+      }, (error) => {
+          //setIsLoaded(true);
+          setError(error);
+        }));
+      }
+      Promise.all(promises).then(() => {
+            setAllFiles(files);
+            setIsLoaded(true);
+          });
+  }
   
   useEffect(() => {
 
     var files = [];
-    var fileUrl = '';
+    
     var promises = [];
-    var toBreak = false;
+    
     /*
     axios.get(`https://random.dog/woof.json`)
       .then(res => { 
@@ -48,51 +80,32 @@ export default function App() {
 
     
     //console.log(files.length);
-    for (let i=0; i<8;i++) {
-    promises.push(axios.get(`https://random.dog/woof.json`)
-      .then(res => { 
-        //setIsLoaded(true); 
-        //setFileUrl(res.data.url);
-        //console.log(fileUrl);
-        fileUrl = res.data.url;
-        console.log(fileUrl);
-        console.log(getFileType(fileUrl));
-        if (fileTypes.includes(getFileType(fileUrl))) {
-          files.push(fileUrl);
-          console.log(files);
-        }
-        console.log(files.length);
-        
-      }, (error) => {
-          //setIsLoaded(true);
-          setError(error);
-        }));
-        
-    }  
-       Promise.all(promises).then(() => {
-          setAllFiles(files);
-          setIsLoaded(true);
-        });
+
+      fetchFiles(promises, files, INITIAL_NUM); 
+    
           
   }, [setAllFiles]);
 
   console.log(allFiles);
   //var 
-  if (allFiles.length < 8){
-    
+  useEffect(() => {
+    if (isLoaded){
+    var files = allFiles.slice(0);
+    var len = files.length;
+    console.log(len);
+    var promises = [];
+    if (len < INITIAL_NUM){
+      fetchFiles(promises, files, INITIAL_NUM - len); 
 
+    }
   }
+  }
+  );
 
   //console.log(fileUrl);
 
   //console.log(allFiles);
 
-  /*{imageTypes.includes(getFileType(allFiles[0])) && <img src={allFiles[0]} style={{height: 300, width: 400}} ></img>}
-          {videoTypes.includes(getFileType(allFiles[0])) && 
-          <video width="320" height="240" controls >
-            <source src={allFiles[0]} />
-            Your browser does not support the video tag.
-          </video>} */
   
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -129,8 +142,7 @@ export default function App() {
           </div>
         )}
         </div>
-        
-
+        <Button type="primary" shape="round" size="large" onClick={() => fetchFiles([], allFiles.slice(0), INITIAL_NUM)}>Load more</Button>
       </div>
     </Content>
     <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
